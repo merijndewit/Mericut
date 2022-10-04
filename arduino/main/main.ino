d877081a6cf3ba9688bb5295c9a8a458b0112a67
merijndewit <merijn.de.wit@gmail.com>
2022-10-04 14:32:55 +0200

    added seperate files for handeling micro Gcode (tmp name)

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,14 +1,62 @@
-int x;
+#include "Arduino.h"
+#include "microGcode.h"
 
-void setup() 
-{
- Serial.begin(115200);
- Serial.setTimeout(1);
+const byte numChars = 32;
+char receivedChars[numChars];
+
+boolean newData = false;
+
+MicroGcode microGcode;
+
+void setup() {
+    Serial.begin(115200);
 }
 
-void loop() 
+void loop() {
+    checkSerial();
+    checkData();
+}
+
+void checkSerial() {
+    static boolean receiving = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char data;
+ 
+    while (Serial.available() > 0 && newData == false) {
+        data = Serial.read();
+
+        if (receiving == true) {
+            if (data != endMarker) {
+                receivedChars[ndx] = data;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0';
+                receiving = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (data == startMarker) {
+            receiving = true;
+        }
+    }
+}
+
+void checkData() {
+    if (newData == true) {
+        executeCommand(receivedChars);
+        newData = false;
+    }
+}
+
+void executeCommand(char* receivedChars) 
 {
- while (!Serial.available());
- x = Serial.readString().toInt();
- Serial.print(x + 1);
+    microGcode.executeMiniGcode(receivedChars);
 }
