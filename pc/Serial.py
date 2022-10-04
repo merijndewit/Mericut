1281fb0d6bcc6a0aa169205a9dbbe33dbdce09c3
merijndewit <merijn.de.wit@gmail.com>
2022-10-04 10:21:49 +0200

    added a serial listener

--- pc/Serial.py
+++ pc/Serial.py
@@ -1,6 +1,8 @@
 from serial.tools import list_ports
 import serial
 import time
+import select
+import threading
 
 class Serial:
     def GetPortNames():
@@ -18,6 +20,7 @@ class Serial:
         if self.selectedComPort == None:
             return
         self.connectedDevice = serial.Serial(port=self.selectedComPort, baudrate=115200, timeout=.1)
+        self.StartListeningToSerial()
 
     def TestConnection(self):
         callback = self.WriteToSerial("<D0>")
@@ -28,4 +31,18 @@ class Serial:
         time.sleep(0.05)
         data = self.connectedDevice.readline()
         return data
+
+    def StartListeningToSerial(self):
+        child_thread = threading.Thread(target=self.ListenToSerial)
+        child_thread.start()
+
+    def ListenToSerial(self):
+        while True:
+            time.sleep(0.5)
+
+            self.connectedDevice.flush()
+            answer=""
+            while  self.connectedDevice.inWaiting() > 0:
+                answer += self.serial.readline()
+            return answer  
         
