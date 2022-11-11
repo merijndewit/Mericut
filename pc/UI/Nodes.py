47ddb3306e96f5ff409e992aa10480594d68e1dd
merijndewit <merijn.de.wit@gmail.com>
2022-11-11 15:06:19 +0100

    Now has offset position and scale for the layers

--- pc/UI/Nodes.py
+++ pc/UI/Nodes.py
@@ -2,8 +2,9 @@ from UI.Colors import Colors
 
 
 class Node():
-    def __init__(self, x, y):
-        self.position = [x, y]
+    def __init__(self, x, y, layer):
+        self.position = [x / layer.scale, y / layer.scale]
+        self.offsetPosition = [0, 0]
         self.connectedNodes = []
         self.shape = None
     
@@ -15,14 +16,30 @@ class Node():
 
     def SetShape(self, shape):
         self.shape = shape
+
+    def SetScale(self, scaleX, refX, scaleY, refY):
+        self.offsetPosition = [(self.position[0] - refX) * scaleX, (self.position[1] - refY) * scaleY]
+        self.UpdateShape()
+
+    def GetPosition(self):
+        return [self.GetPositionX(), self.GetPositionY()]
+
+    def GetPositionY(self):
+        return self.position[1] + self.offsetPosition[1]
+
+    def GetPositionX(self):
+        return self.position[0] + self.offsetPosition[0]
+    
     @staticmethod
     def GetColisionColor():
         return Colors.COLISIONNODE
 
 class MergedNode():
-    def __init__(self, position, nodesToMerge):
-        self.position = position
+    def __init__(self, position, nodesToMerge, layer):
+        self.position = [position[0] / layer.scale, [1] / layer.scale]
         self.shapes = []
+        self.offsetPosition = [0, 0]
+        
         for i in range(len(nodesToMerge)):
             self.AddNode(nodesToMerge[i])
 
@@ -41,6 +58,20 @@ class MergedNode():
 
     def SetShape(self, shape):
         self.shapes.append(shape)
+
+    def SetScale(self, scaleX, refX, scaleY, refY):
+        self.offsetPosition = [(self.position[0] - refX) * scaleX, (self.position[1] - refY) * scaleY]
+        self.UpdateShape()
+
+    def GetPosition(self):
+        return [self.GetPositionX(), self.GetPositionY()]
+
+    def GetPositionY(self):
+        return self.position[1] + self.offsetPosition[1]
+
+    def GetPositionX(self):
+        return self.position[0] + self.offsetPosition[0]
+
     @staticmethod
     def GetColisionColor():
         return Colors.COLISIONMERGEDNODE
