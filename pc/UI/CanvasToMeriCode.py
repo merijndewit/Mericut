6f58dded990228b3631a27c3cd069848c0635e1b
merijndewit <merijn.de.wit@gmail.com>
2022-11-15 15:26:27 +0100

    now has cutting mode with support for tools with offsets

--- pc/UI/CanvasToMeriCode.py
+++ pc/UI/CanvasToMeriCode.py
@@ -1,30 +1,31 @@
+import math
 class CanvasToMeriCode:
     def __init__(self, canvas):
         self.position = [0, 0]
         self.canvas = canvas
         self.mergeDistance = 0.04
+        self.cutting = True
+        self.toolOffsetRadius = 4
         with open('Test/MeriCodeTestFile.txt', "w") as file:
             for layer in range(len(self.canvas.layers)):
                 for i in range(len(self.canvas.layers[layer].drawnShapes)):
-                    shapeStartPosition = self.canvas.layers[layer].drawnShapes[i].GetStartPosition()
-                    shapeEndPosition = self.canvas.layers[layer].drawnShapes[i].GetEndPosition()
-                    if (abs(self.position[0] - shapeStartPosition[0]) <= self.mergeDistance and abs(self.position[1] - shapeStartPosition[1]) <= self.mergeDistance):
-                        self.DrawShapeReversed(file, self.canvas.layers[layer].drawnShapes[i].lines)
-                        continue
-                    if (abs(self.position[0] - shapeEndPosition[0]) <= self.mergeDistance and abs(self.position[1] - shapeEndPosition[1]) <= self.mergeDistance):
-                        self.DrawShape(file, self.canvas.layers[layer].drawnShapes[i].lines)
-                        continue
-                    self.MoveTo(file, shapeStartPosition)
+                    #shapeStartPosition = self.canvas.layers[layer].drawnShapes[i].GetStartPosition()
+                    #shapeEndPosition = self.canvas.layers[layer].drawnShapes[i].GetEndPosition()
+                    #if (abs(self.position[0] - shapeStartPosition[0]) <= self.mergeDistance and abs(self.position[1] - shapeStartPosition[1]) <= self.mergeDistance):
+                    #    self.DrawShapeReversed(file, self.canvas.layers[layer].drawnShapes[i].lines)
+                    #    continue
+                    #if (abs(self.position[0] - shapeEndPosition[0]) <= self.mergeDistance and abs(self.position[1] - shapeEndPosition[1]) <= self.mergeDistance):
+                    #    self.DrawShape(file, self.canvas.layers[layer].drawnShapes[i].lines)
+                    #    continue
                     self.DrawShape(file, self.canvas.layers[layer].drawnShapes[i].lines)
             self.MoveToolUp(file)
             self.MoveToHome(file)
             file.close()
 
-    def MoveTo(self, file, position):
+    def TravelTo(self, file, position):
         self.MoveToolUp(file)
-        file.write("<M0 X" + str(round(position[0], 4)) + " Y" + str(round(position[1], 4)) + ">" + "\n")
+        self.MoveXY(file, position[0], position[1], 4)
         self.position = position
-        self.MoveToolDown(file)
 
     def DrawShape(self, file, lines):
         for line in range(len(lines)):
@@ -35,26 +36,79 @@ class CanvasToMeriCode:
             self.WriteMeriCodeLine(file, line.x0, line.y0, line.x1, line.y1)
 
     def WriteMeriCodeLine(self, file, x0, y0, x1, y1):
-        if (abs(self.position[0] - x0) <= self.mergeDistance and abs(self.position[1] - y0) <= self.mergeDistance):
-            file.write("<M0 X" + str(round(x1, 4)) + " Y" + str(round(y1, 4)) + ">" + "\n") #move the tool to the start of the line
-            self.position = [x1, y1]
-            return
-        if (abs(self.position[0] - x1) <= self.mergeDistance and abs(self.position[1] - y1) <= self.mergeDistance):
-            file.write("<M0 X" + str(round(x0, 4)) + " Y" + str(round(y0, 4)) + ">" + "\n") #move the tool to the start of the line
-            self.position = [x0, y0]
-            return
-        self.MoveToolUp(file)
-        file.write("<M0 X" + str(round(x0, 4)) + " Y" + str(round(y0, 4)) + ">" + "\n") #move the tool to the start of the line
+        #if (abs(self.position[0] - x0) <= self.mergeDistance and abs(self.position[1] - y0) <= self.mergeDistance): #check if first point of line is equal with the current position
+        #    #create line from start to end
+        #    offset = [0, 0]
+        #    if self.cutting:
+        #        self.RotateTool(file, self.GetAngle(y0 - y1, x0 - x1), 4)
+        #        offset = self.GetOffsetPosition(self.toolOffsetRadius, self.GetAngle(y0 - y1, x0 - x1))
+#
+        #    self.MoveToolDown(file)
+        #    self.MoveXY(file, x1 + offset[0], y1 + offset[1], 4)
+        #    return
+#
+        #if (abs(self.position[0] - x1) <= self.mergeDistance and abs(self.position[1] - y1) <= self.mergeDistance): #check if the last point of the line is equal to the current position
+        #    #reverse create a line from end to start
+        #    offset = [0, 0]
+        #    if self.cutting:
+        #        self.RotateTool(file, self.GetAngle(y1 - y0, x1 - x0), 4)
+        #        offset = self.GetOffsetPosition(self.toolOffsetRadius, self.GetAngle(y1 - y0, x1 - x0))
+#
+        #    self.MoveToolDown(file)
+        #    self.MoveXY(file, x0 + offset[0], y0 + offset[1], 4)
+        #    return
+
+        #if current position isn't equal to the start or end of the line then travel there
+        offset = [0, 0]
+        if self.cutting:
+            self.MoveToolUp(file)
+            self.RotateTool(file, self.GetAngle(y0 - y1, x0 - x1), 4)
+            offset = self.GetOffsetPosition(self.toolOffsetRadius, self.GetAngle(y0 - y1, x0 - x1))
+        self.TravelTo(file, [x0 + offset[0], y0 + offset[1]])
         self.MoveToolDown(file)
-        file.write("<M0 X" + str(round(x1, 4)) + " Y" + str(round(y1, 4)) + ">" + "\n") #move the tool to the start of the line
-        self.position = [x1, y1]
-    @staticmethod
-    def MoveToolUp(file):
+        self.MoveXY(file, x1 + offset[0], y1 + offset[1], 4)
+
+    
+    def MoveToolUp(self, file):
         file.write("<M0 Z" + str(20) + ">" + "\n")
-    @staticmethod
-    def MoveToolDown(file):
+
+    def MoveToolDown(self, file):
         file.write("<M0 Z" + str(0) + ">" + "\n")
-    @staticmethod
-    def MoveToHome(file):
+    
+    def MoveToHome(self, file):
         file.write("<M0 X0 Y0>" + "\n") #move the tool in the material
-        CanvasToMeriCode.MoveToolDown(file)
+        self.MoveToolDown(file)
+    def MoveXY(self, file, x, y, ndigits):
+        file.write("<M0 X" + str(round(x, ndigits)) + " Y" + str(round(y, ndigits)) + ">" + "\n")
+        self.position = [x, y]
+
+    def RotateTool(self, file, degrees, ndigits):
+        file.write("<M0 T" + str(round(degrees, ndigits)) + ">" + "\n")
+
+    @staticmethod
+    def GetAngle(point0, point1):
+        angle = math.degrees(math.atan2(point0, point1))
+        if angle < 0:
+            angle += 360
+        return angle
+
+    #this is for when the tool can rotate and the centerpoint of the tool changes based on its rotation
+    @staticmethod
+    def GetOffsetPosition(radius, angle):
+        invert = True
+        angle = math.radians(angle)
+
+        x = radius * math.sin(angle)
+        if invert and x < 0:
+            x = abs(x)
+        else:
+            x = -abs(x)
+
+        y = radius * math.cos(angle)
+        if invert and y < 0:
+            y = abs(y)
+        else:
+            y = -abs(y)
+
+        print([x, y])
+        return [y, x]
