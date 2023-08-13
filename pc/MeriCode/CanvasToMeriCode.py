f29e4b4e31644a967d82ec07124ae73191ed093a
merijndewit <merijn.de.wit@gmail.com>
2023-08-13 22:49:58 +0200

    simplefied drawing mericode line and calculating path order now works correctly

--- pc/MeriCode/CanvasToMeriCode.py
+++ pc/MeriCode/CanvasToMeriCode.py
@@ -5,7 +5,7 @@ class CanvasToMeriCode:
         self.position = [0, 0]
         self.rotation = 0
         self.canvas = canvas
-        self.mergeDistance = 0.05
+        self.mergeDistance = 0.5
         self.cutting = cutting
         self.toolOffsetRadius = 3.75
         self.numberOfCuts = 1
@@ -22,25 +22,82 @@ class CanvasToMeriCode:
         with open('Test/MeriCodeTestFile.txt', "w") as file:
             for cut in range(self.numberOfCuts):
                 self.currentCut = cut
+
                 for layer in range(len(self.canvas.layers)):
-                    for i in range(len(self.canvas.layers[layer].drawnShapes)):
-                        shapeStartPosition = self.canvas.layers[layer].drawnShapes[i].GetStartPosition()
-                        shapeEndPosition = self.canvas.layers[layer].drawnShapes[i].GetEndPosition()
+                    shapesToDraw = self.CalculateNewPathOrder(self.canvas.layers[layer].drawnShapes)
+
+                    for i in range(len(shapesToDraw)):
+                        print("startPosition: X: " + str(shapesToDraw[i].GetStartPosition()[0]) + "Y: " + str(shapesToDraw[i].GetStartPosition()[1]))
+                        print("endPosition: X: " + str(shapesToDraw[i].GetEndPosition()[0]) + "Y: " + str(shapesToDraw[i].GetEndPosition()[1]))
+                        shapeEndPosition = shapesToDraw[i].GetEndPosition()
+                        nextShapeEndPosition = [0, 0]
+                        nextShapeStartPosition = [0, 0]
+                        if len(shapesToDraw) - 1 != i:
+                            nextShapeEndPosition = shapesToDraw[i + 1].GetEndPosition()
+                            nextShapeStartPosition = shapesToDraw[i + 1].GetStartPosition()
+
                         offset = [0, 0]
                         if self.cutting:
                             offset = self.GetOffsetPosition(self.toolOffsetRadius, self.rotation)
-                        if (abs((self.position[0] - offset[0]) - shapeStartPosition[0]) <= self.mergeDistance and abs((self.position[1] - offset[1])  - shapeStartPosition[1]) <= self.mergeDistance):
-                            self.DrawShapeReversed(file, self.canvas.layers[layer].drawnShapes[i].lines)
+
+                        if (abs(self.position[0] - nextShapeEndPosition[0]) <= self.mergeDistance and abs(self.position[1] - nextShapeEndPosition[1]) <= self.mergeDistance):
+                            self.DrawShapeReversed(file, shapesToDraw[i].lines)
                             continue
-                        if (abs((self.position[0] - offset[0]) - shapeEndPosition[0]) <= self.mergeDistance and abs((self.position[1] - offset[1]) - shapeEndPosition[1]) <= self.mergeDistance):
-                            self.DrawShape(file, self.canvas.layers[layer].drawnShapes[i].lines)
+                        if (abs(self.position[0] - nextShapeStartPosition[0]) <= self.mergeDistance and abs(self.position[1] - nextShapeStartPosition[1]) <= self.mergeDistance):
+                            self.DrawShape(file, shapesToDraw[i].lines)
                             continue
-                        self.DrawShape(file, self.canvas.layers[layer].drawnShapes[i].lines)
+
+                        self.DrawShapeReversed(file, shapesToDraw[i].lines)
+
+
             self.currentCut = 0
             self.MoveToolUp(file)
             self.MoveToHome(file)
 
             file.close()
+
+    def CalculateNewPathOrder(self, shapes):
+        newShapeOrder = []
+        visitedShapesIndex = []
+        currentPosition = None
+
+        def CheckForSamePosition(currentPosition):
+            for i in range(len(shapes)):
+                if i in visitedShapesIndex:
+                    continue
+
+                shapeStartPosition = shapes[i].GetStartPosition()
+                shapeEndPosition = shapes[i].GetEndPosition()
+                maxMergeDistance = 1
+                if abs(shapeStartPosition[0] - currentPosition[0]) <= maxMergeDistance and abs(shapeStartPosition[1] - currentPosition[1]) <= maxMergeDistance:
+
+                    currentPosition = shapes[i].GetEndPosition()
+                    visitedShapesIndex.append(i)
+                    newShapeOrder.append(shapes[i])
+                    continue
+                elif abs(shapeEndPosition[0] - currentPosition[0]) <= maxMergeDistance and abs(shapeEndPosition[1] - currentPosition[1]) <= maxMergeDistance:
+
+                    currentPosition = shapes[i].GetStartPosition()
+                    visitedShapesIndex.append(i)
+                    newShapeOrder.append(shapes[i])
+                    continue
+
+            else: #coudn't find shape that starts or ends on current position 
+                for i in range(len(shapes)):
+                    if not i in visitedShapesIndex:
+                        currentPosition = shapes[i].GetEndPosition()
+                        visitedShapesIndex.append(i)
+                        newShapeOrder.append(shapes[i])
+                        break
+
+        currentPosition = shapes[0].GetStartPosition()
+        visitedShapesIndex.append(0)
+        newShapeOrder.append(shapes[0])
+
+        while len(shapes) != len(visitedShapesIndex):
+            CheckForSamePosition(currentPosition)
+
+        return newShapeOrder
 
     def TravelTo(self, file, position):
         self.MoveToolUp(file)
@@ -50,61 +107,19 @@ class CanvasToMeriCode:
     def DrawShape(self, file, lines):
         self.shapes += 1
         for line in range(len(lines)):
-            self.WriteMeriCodeLine(file, self.canvas.CanvasPosXToNormalPosX(lines[line].x0), self.canvas.CanvasPosYToNormalPosY(lines[line].y0), self.canvas.CanvasPosXToNormalPosX(lines[line].x1), self.canvas.CanvasPosYToNormalPosY(lines[line].y1))
+            self.WriteMeriCodeLine(file, [self.canvas.CanvasPosXToNormalPosX(lines[line].x0), self.canvas.CanvasPosYToNormalPosY(lines[line].y0)], [self.canvas.CanvasPosXToNormalPosX(lines[line].x1), self.canvas.CanvasPosYToNormalPosY(lines[line].y1)])
 
     def DrawShapeReversed(self, file, lines):
         self.shapes += 1
-        for line in reversed(lines):
-            self.WriteMeriCodeLine(file, self.canvas.CanvasPosXToNormalPosX(line.x0), self.canvas.CanvasPosYToNormalPosY(line.y0), self.canvas.CanvasPosXToNormalPosX(line.x1), self.canvas.CanvasPosYToNormalPosY(line.y1))
+        for line in reversed(range(len(lines))):
+            self.WriteMeriCodeLine(file, [self.canvas.CanvasPosXToNormalPosX(lines[line].x1), self.canvas.CanvasPosYToNormalPosY(lines[line].y1)], [self.canvas.CanvasPosXToNormalPosX(lines[line].x0), self.canvas.CanvasPosYToNormalPosY(lines[line].y0)])
 
-    def WriteMeriCodeLine(self, file, x0, y0, x1, y1):
+    def WriteMeriCodeLine(self, file, lineStart, lineEnd):
         self.lines += 1
-        offsetTool = [0, 0]
-        if self.cutting:
-            offsetTool = self.GetOffsetPosition(self.toolOffsetRadius, self.rotation)
-        if (abs((self.position[0] - offsetTool[0]) - x0) <= self.mergeDistance and abs((self.position[1] - offsetTool[1]) - y0) <= self.mergeDistance): #check if first point of line is equal with the current position
-            #create line from start to end
-            offset = [0, 0]
-            if self.cutting:
-                angle = self.GetAngle(y0 - y1, x0 - x1)
-                moveToolDown = False
-                if abs(self.rotation - angle) > self.continueLineAngle:
-                    self.MoveToolUp(file)
-                    moveToolDown = True
-                offset = self.GetOffsetPosition(self.toolOffsetRadius, self.GetAngle(y0 - y1, x0 - x1))
-                oldOffset = self.GetOffsetPosition(self.toolOffsetRadius, self.rotation)
-                self.MoveXYT(file, (self.position[0] - oldOffset[0]) + offset[0], (self.position[1] - oldOffset[1]) + offset[1], self.GetAngle(y0 - y1, x0 - x1), 4)
-                if moveToolDown:
-                    self.MoveToolDown(file)
-            self.MoveXY(file, x1 + offset[0], y1 + offset[1], 4)
-            return
-
-        if (abs((self.position[0] - offsetTool[0]) - x1) <= self.mergeDistance and abs((self.position[1] - offsetTool[1]) - y1) <= self.mergeDistance): #check if the last point of the line is equal to the current position
-            offset = [0, 0]
-            if self.cutting:
-                angle = self.GetAngle(y1 - y0, x1 - x0)
-                moveToolDown = False
-                if abs(self.rotation - angle) > self.continueLineAngle:
-                    self.MoveToolUp(file)
-                    moveToolDown = True
-                offset = self.GetOffsetPosition(self.toolOffsetRadius, self.GetAngle(y1 - y0, x1 - x0))
-                oldOffset = self.GetOffsetPosition(self.toolOffsetRadius, self.rotation)
-                self.MoveXYT(file, (self.position[0] - oldOffset[0]) + offset[0], (self.position[1] - oldOffset[1]) + offset[1], self.GetAngle(y1 - y0, x1 - x0), 4)
-                if moveToolDown:
-                    self.MoveToolDown(file)
-            self.MoveXY(file, x0 + offset[0], y0 + offset[1], 4)
-            return
-
-        #if current position isn't equal to the start or end of the line then travel there
-        offset = [0, 0]
-        if self.cutting:
-            self.MoveToolUp(file)
-            self.RotateTool(file, self.GetAngle(y0 - y1, x0 - x1), 4)
-            offset = self.GetOffsetPosition(self.toolOffsetRadius, self.GetAngle(y0 - y1, x0 - x1))
-        self.TravelTo(file, [x0 + offset[0], y0 + offset[1]])
+        if abs(lineStart[0] - self.position[0]) >= self.mergeDistance and abs(lineStart[1] - self.position[1]) >= self.mergeDistance:
+            self.TravelTo(file, [lineStart[0], lineStart[1]])
         self.MoveToolDown(file)
-        self.MoveXY(file, x1 + offset[0], y1 + offset[1], 4)
-
+        self.MoveXY(file, lineEnd[0], lineEnd[1], 4)
     
     def MoveToolUp(self, file):
         self.travels += 1
